8cc2855ab213c5af96f62f126a5dfa6a481f9812
Arash Mousavi <mousavi.arash@gmail.com>
2017-11-17 22:25:36 +0330

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,14 @@
 const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
   entry: {
-    'jdate': __dirname + '/src/jdate.js',
-    'jdate.min': __dirname + '/src/jdate.js'
+    'jdate': path.join(__dirname, '/src/jdate.js'),
+    'jdate.min': path.join(__dirname, '/src/jdate.js')
   },
   devtool: 'source-map',
   output: {
-    path: __dirname + '/lib',
+    path: path.join(__dirname, '/lib'),
     filename: '[name].js',
     library: 'jdate',
     libraryTarget: 'umd',
@@ -15,8 +16,8 @@ module.exports = {
   },
   module: {
     loaders: [
-      {exclude: ['node_modules'], loader: 'babel-loader', test: /\.js$/},
-    ],
+      { exclude: ['node_modules'], loader: 'babel-loader', test: /\.js$/ }
+    ]
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin({
