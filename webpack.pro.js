07b08d0ec502dd1bb3e4feab731efb124c875c8b
Arash Mousavi <mousavi.arash@gmail.com>
2019-08-24 20:47:56 +0100

    Add a separate minified file

--- webpack.pro.js
+++ webpack.pro.js
@@ -2,5 +2,8 @@ const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+  output: {
+    filename: '[name].min.js'
+  },
   mode: 'production'
 });
